6e3931c732b0224c753b7329ace249911d981ba6
Manoj Kumar <musalannagarimanoj1999@gmail.com>
2023-08-06 15:30:00 +0530

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# mail-lambda-dynamodb
-This repo has the required guide to send an email whenever an entry drops in the dynamodb table.
+# dynamodb-trigger-lambda-SES
+Whenever there is an entry in dynamo_db, lambda gets triggered and sends out an email with the required information.
+
+# Architecture
+
+![Alt text](image.png)
+
+
+# Set up
+1.Analyze the AWS Amplify and AppSync setup. (Process of pushing data into the Dynamo-db Table).
+2.Examine the format and schema of data.
+3.Create a lambda and provide LambdaExecutionRole with DynamoDBFullAceess to achieve integration.
+4.Configure the trigger as Dynamo-db Table.
+5.To achieve lambda integration with SES, provide SESFullAccess to LambdaExecutionRole.
+6.Add identities in SES i.e., source and destination mail addresses.
+7.Make sure to verify the mail address by confirming the URL in the mail which has been sent by SES.
+8.Now hop on the lambda we have created, write logic to fetch the data from Dynamo Db Table and send the email context to SES.
+9.Post that SES sends out the mail to the destination mail address.
+10.Insert a sample data in the Dynamo-db table that triggers the lambda and sends out the mail.
+11.You will receive the e-mail in few seconds.
+12.Reference - 1) https://betterprogramming.pub/send-emails-serverlessly-with-node-js-lambda-and-aws-ses-186cba40d695 2) https://blog.focusotter.com/serverless-contact-form-using-aws-amplify
+
+
+Happy mailing!!!!!
--- lambda-script.js
+++ lambda-script.js
@@ -3,31 +3,21 @@ const SES = new AWS.SES();
 // const fs = require('fs');
 
 exports.handler = async event => {
-    // // console.log("DB ", event.Records[0].dynamodb.NewImage.key.S);
-    // // console.log("DB-1", event.Records[0].dynamodb.NewImage.key);
-    // // console.log("DB-2", event.Records[0].dynamodb.NewImage);
-    // console.log("DB-3", event.Records[0].dynamodb);
-    // return {
-    //         statusCode: 200,
-    //         body: 'Testing!'
-    //     }
-    // const { to, subject } = JSON.parse(event.body);
-    // const body = fs.readFileSync('./body.html').toString()
     const body = `<div>User :${event.Records[0].dynamodb.NewImage.firstName.S + " " + event.Records[0].dynamodb.NewImage.lastName.S } <br> Email : ${event.Records[0].dynamodb.NewImage.emailAddress.S} <br> ${event.Records[0].dynamodb.NewImage.feedbackMessage.S} </div>` ;
 
     const params = {
         Destination: {
-            ToAddresses: ["manumanojkumar27@gmail.com"],
+            ToAddresses: ["<email-address>"],
         },
         Message: {
             Body: {
                 Html: { Data: body }
             },
             Subject: {
-                Data: "Trace Feedback from " + event.Records[0].dynamodb.NewImage.firstName.S
+                Data: "Feedback from " + event.Records[0].dynamodb.NewImage.firstName.S
             },
         },
-        Source: 'manoj@makoitlab.com'
+        Source: '<email-address>'
     };
 
     try {
